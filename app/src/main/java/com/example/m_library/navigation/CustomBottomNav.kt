eb8b2abd365a4a25381681a65151f4c29c562570
joykangangi <64706463+joykangangi@users.noreply.github.com>
2023-03-28 17:24:44 +0300

    Hide BottomNav in Detail Screen

--- app/src/main/java/com/example/m_library/navigation/CustomBottomNav.kt
+++ app/src/main/java/com/example/m_library/navigation/CustomBottomNav.kt
@@ -1,19 +1,26 @@
 package com.example.m_library.navigation
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.clickable
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.m_library.R
 
@@ -22,20 +29,37 @@ import com.example.m_library.R
 fun CustomBottomNav() {
 
     val navController = rememberNavController()
+    val bottomBarState = rememberSaveable { (mutableStateOf(true))}
 
+    // Subscribe to navBackStackEntry, required to get current route
+    val navStackBackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navStackBackEntry?.destination
+
+    when (currentDestination?.route) {
+        BottomScreen.MyBooks.route -> bottomBarState.value = true
+        BottomScreen.Stats.route -> bottomBarState.value = true
+        else -> bottomBarState.value = false
+    }
+    
     Scaffold(
         scaffoldState = rememberScaffoldState(),
         bottomBar = {
-            BottomAppBar(
-                backgroundColor = MaterialTheme.colors.primary,
-                cutoutShape = CircleShape
-            ) {
-                CustomBottomBar(navController = navController)
+            if (bottomBarState.value) {
+                BottomAppBar(
+                    backgroundColor = MaterialTheme.colors.primary,
+                    cutoutShape = CircleShape
+                ) {
+                    CustomBottomBar(
+                        navController = navController,
+                        bottomBarState = bottomBarState.value,
+                        currentDestination = currentDestination
+                    )
+                }
             }
         },
         floatingActionButtonPosition = FabPosition.Center,
         isFloatingActionButtonDocked = true,
-        floatingActionButton = { FabButton{
+        floatingActionButton = { FabButton(bottomBarState = bottomBarState.value){
             navController.navigate(Screen.AddBook.route)
         } },
     ) {
@@ -43,77 +67,65 @@ fun CustomBottomNav() {
     }
 }
 
-
 @Composable
-fun FabButton(onFabClick:() -> Unit) {
-    FloatingActionButton(
-        onClick = onFabClick,
-        backgroundColor = MaterialTheme.colors.primaryVariant,
-        contentColor = MaterialTheme.colors.background,
-        elevation = FloatingActionButtonDefaults.elevation(2.dp, 3.dp)
-    ) {
-        Icon(imageVector = Icons.Default.Add, contentDescription = stringResource(id = R.string.add_book) )
+fun FabButton(bottomBarState: Boolean,onFabClick:() -> Unit) {
+    if (bottomBarState) {
+        FloatingActionButton(
+            onClick = onFabClick,
+            backgroundColor = MaterialTheme.colors.primaryVariant,
+            contentColor = MaterialTheme.colors.background,
+            elevation = FloatingActionButtonDefaults.elevation(2.dp, 3.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = stringResource(id = R.string.add_book)
+            )
+        }
     }
-
 }
 
 @Composable
 fun CustomBottomBar(
-    navController: NavHostController
+    navController: NavHostController,
+    bottomBarState: Boolean,
+    currentDestination: NavDestination?
 ) {
-
     val bottomScreens: List<BottomScreen> = listOf(
         BottomScreen.MyBooks,
         BottomScreen.Stats
     )
 
-    //val navStackBackEntry by navController.currentBackStackEntryAsState()
-  //val currentDestination = navStackBackEntry?.destination
+    Log.i("CUSTOM BOTTOM NAV - Bottom Bar State", "$bottomBarState")
 
-    Row(
-        modifier = Modifier
-            .padding(3.dp)
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically
+    AnimatedVisibility(
+        visible = bottomBarState,
+        enter = fadeIn(),
+        exit = fadeOut()
     ) {
+        BottomNavigation {
 
-        bottomScreens.forEach { screen ->
-            BottomNavItem(
-                bottomScreen = screen,
-                navController = navController
-            )
+            bottomScreens.forEach { screen ->
+                val isSelected = currentDestination?.hierarchy?.any { it.route == screen.route } == true
+                BottomNavigationItem(
+                    icon =  {
+                        Icon(
+                            imageVector = screen.icon,
+                            contentDescription = screen.title,
+                            tint = MaterialTheme.colors.onPrimary
+                        )
+                    },
+                    label =  { Text(text = screen.title, color = MaterialTheme.colors.onPrimary)},
+                    selected = isSelected,
+                    onClick = {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id){ saveState = true}
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                )
+            }
         }
     }
 }
 
-
-@Composable
-fun BottomNavItem(
-    bottomScreen: BottomScreen,
-    navController: NavHostController
-) {
-    //val isSelected = currentDestination?.hierarchy?.any { it.route == bottomScreen.route } == true
-
-    Box(
-        modifier = Modifier
-            .clickable(onClick = {
-                navController.navigate(bottomScreen.route) {
-                    popUpTo(navController.graph.findStartDestination().id)
-                    launchSingleTop = true
-                }
-            })
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Icon(
-                imageVector = bottomScreen.icon,
-                contentDescription = bottomScreen.title,
-                tint = MaterialTheme.colors.onPrimary
-            )
-            Text(text = bottomScreen.title, color = MaterialTheme.colors.onPrimary)
-        }
-
-    }
-}
